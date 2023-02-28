c6175dd1158ddbd5719f1897d7354456de9b921f
apillacag <126400110+apillacag@users.noreply.github.com>
2023-02-27 23:03:03 -0500

    Create Ejercicio4.py

---
+++ ListaEjercicios1/Ejercicio4.py
@@ -0,0 +1,14 @@
+#Ejemplo1
+valor = input("Ingrese un valor: ")
+print(type(valor))
+
+#Ejemplo2
+entero = 8
+flotante = 2.4
+booleano = True
+lineadecadena = "Hola Mundo"
+print(type(entero))
+print(type(flotante))
+print(type(booleano))
+print(type(lineadecadena))
+

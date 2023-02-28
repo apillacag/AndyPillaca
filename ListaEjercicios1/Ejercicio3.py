2433ddc23f64abd45c10b5529358959884cdf575
apillacag <126400110+apillacag@users.noreply.github.com>
2023-02-27 22:44:55 -0500

    Create Ejercicio3.py

---
+++ ListaEjercicios1/Ejercicio3.py
@@ -0,0 +1,13 @@
+valor1 = int(input("Ingrese el valor 1: "))
+valor2 = int(input("Ingrese el valor 2: "))
+valor3 = int(input("Ingrese el valor 3: "))
+multiplicacion = valor1 * valor2 * valor3
+suma = valor1 + valor2 + valor3
+resta = abs(valor3 - valor2 - valor1)
+div = (valor1/valor2)/valor3
+divEnt = (valor1//valor2)//valor3
+print("Multiplicacion: ", multiplicacion)
+print("suma: ", suma)
+print("Resta: ", resta)
+print("Division: ", div)
+print("Division Entera: ", divEnt)
